2552018af93c4e5df6952e373c809589f5ba9716
Piotr Łądkowski <piolad2003@gmail.com>
2024-02-06 20:00:30 +0000

    inital commit

---
+++ valentine/src/App.js
@@ -0,0 +1,12 @@
+import "./components/navbar.js"
+import './App.css';
+import Nabvar from './components/navbar';
+function App() {
+  return (
+    <div className="App">
+      <Nabvar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ valentine/src/components/navbar.js
@@ -0,0 +1,9 @@
+function Nabvar() {
+  return (
+    <div className="nabvar">
+      <p>nav</p>
+    </div>
+  );
+}
+
+export default Nabvar;
